fa09db1ff4747565b7e32fccae716b0095dfb1a3
Steven Woo <swoo9875@gmail.com>
2021-03-23 21:09:32 -0400

    adding templates

---
+++ controllers/brewTime.js
@@ -0,0 +1,60 @@
+var express = require("express");
+
+var router = express.Router();
+
+const brewTime = require("../models/brewTime.js");
+
+//router.get
+router.get("/", function(req, res) {
+    brewTime.all(function(data) {
+      var hbsObject = {
+        brewTime: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+//router.post
+router.post("/api/brewTime", function(req, res) {
+    brewTime.create([
+        //
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+//router.put
+router.put("/api/brewTime/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    hamburger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+//router.delete
+router.delete("/api/brewTime/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    hamburger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
