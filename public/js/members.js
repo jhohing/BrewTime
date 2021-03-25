f9b4cf171db9bf993919858aadf8b14edf3347ca
Justin Hohing <jnh0627@yahoo.com>
2021-03-25 19:57:21 -0400

    added Geolocation api

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
+
+    getlocation()
+    function getlocation() {
+      if (window.navigator.geolocation) {
+        (window.navigator.geolocation.getCurrentPosition(locationData =>
+          console.log(locationData)));
+      }
+      else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
   });
 });
