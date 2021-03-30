567bb8d54b727205fc9641ef08c0dd70cf524076
adeha047 <dehaven7777@gmail.com>
2021-03-30 15:53:42 -0400

    debugging

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,35 @@
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+let map, infoWindow;
+function initMap(lat, long) {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: lat, lng: long },
+        zoom: 14,
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    }); 
+    infoWindow = new google.maps.InfoWindow();
 }
+
+async function renderSearches(lon, lat) {
+    var apiKey = "AIzaSyBpP9c6UVQA5-hoA1VcR953lZwAsGfUUFg";
+    var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lon},${lat}&radius=3000&keyword=cafe&type=cafe&key=${apiKey}`;
+    const request = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(data => data.json())
+        .catch(err => console.error(err));
+    console.log(request);
+    request.results.map(function (item, index) {
+        // console.log(index, item.name);
+        $(".results").append(`<p> ${index}: ${item.name} Open: ${item.opening_hours.open_now === true ? "Yes" : "No"} </p>`);
+        // console.log(item.geometry.location.lat, item.geometry.location.lng);
+        const marker = new google.maps.Marker({
+            position: { lat: item.geometry.location.lat, lng: item.geometry.location.lng },
+            label: `${index}`,
+            // zIndex: 1
+
+        })
+        marker.setMap(map)
+    })
+}; 
