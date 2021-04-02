76eb6ccd573cf8ce32851d4bcc8cd0dfb033c7aa
adeha047 <dehaven7777@gmail.com>
2021-04-01 21:37:57 -0400

    fixed a major problem with the server

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,37 +7,25 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    if (req.user) {
-      res.redirect("/index");
-    }
     res.render("index");
   });
 //get the login page
   app.get("/login", function(req, res) {
-    if (req.user) {
-      res.redirect("/login");
-    }
     res.render("login");
   });
 //get the history page
   app.get("/history", function(req, res) {
-    if (req.user) {
-      res.redirect("/history");
-    }
     res.render("history");
   });
 //get the about page
   app.get("/about", function(req, res) {
-    if (req.user) {
-      res.redirect("/about");
-    }
     res.render("about");
   });
 //get the maps page
   app.get("/members", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
+    if (!req.user) {
+      res.redirect("/login");
     }
     res.render("maps");
   });
